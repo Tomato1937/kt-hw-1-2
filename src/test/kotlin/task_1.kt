bf15b231b7f634c0ca7299e5d1784536b3473cf7
Tomato1937 <student@pisem.net>
2022-02-03 09:36:13 +0700

    добавлена Задача №2

--- src/test/kotlin/task_1.kt
+++ src/test/kotlin/task_1.kt
@@ -1,6 +1,8 @@
 fun main() {
     val amount: Int = 100_00;
     val taxMin: Int = 35_00;
+
     val totalTax: Int = if (amount * 0.0075 >= taxMin) (amount * 0.000075).toInt() else taxMin/100;
+
     println("Total Tax: $totalTax руб.");
 }
---
+++ src/test/kotlin/task_2.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val likes: Int = 101;
+
+    if (likes != 11 && likes % 10 == 1) println("Понравилось $likes человеку") else println("Понравилось $likes людям");
+}
